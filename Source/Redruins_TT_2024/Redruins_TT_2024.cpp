a2c0ec9520d897e23ce6d3297a026d7516394ba5
Xatec <xatec@mail.ru>
2024-05-21 09:36:31 +0300

    Init

---
+++ Source/Redruins_TT_2024/Redruins_TT_2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Redruins_TT_2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Redruins_TT_2024, "Redruins_TT_2024" );
